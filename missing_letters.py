092ad038784977a9c669fb95da414c116bbd1c76
Mike <mtefauthor@gmail.com>
2024-02-06 21:09:05 +0000

    Kata 72 completed

--- missing_letters.py
+++ missing_letters.py
@@ -3,8 +3,8 @@ def find_missing_letter(chars):
     for letter in chars[1:]:
         if ord(letter) != (curr + 1):
             return chr(ord(letter)-1)    
-        curr = ord(letter)
+        curr = ord(letter) 
         print(curr)
 
 
-print(find_missing_letter(['a','b','c','d','f'])) # should be 'e'
+#print(find_missing_letter(['a','b','c','d','f'])) # should be 'e'
